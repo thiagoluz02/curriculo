eb7b1db036d535da26009209230004d26289cb46
Thiago"thi"Flores <thiagocalavera@hotmail.com>
2020-02-16 20:24:03 -0300

    Add files via upload

---
+++ fonte.js
@@ -0,0 +1,11 @@
+const ulsquares = document.querySelector("ul.squares");
+
+for(let i = 0;i < 11; i++){
+    const li = document.createElement("li");
+  for(let i =0;i<11;i++){
+      conts size = Math.random()*(120 - 10)
+      li.style.width
+  }
+  
+    ulsquares.appendChild("li"); 
+}
